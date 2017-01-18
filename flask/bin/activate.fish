c90cc0884f42c260c1264ccec95cdb1e088659cc
yangsaerohoon <yangsaerohoon@unionpool.com>
2017-01-18 21:51:49 +0900

    xxxx

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yangsaerohoon/workspace2/computer-science-flash-cards/flask"
+set -gx VIRTUAL_ENV "/Users/saerohoonyang/workspace/computer-science-flash-cards/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
